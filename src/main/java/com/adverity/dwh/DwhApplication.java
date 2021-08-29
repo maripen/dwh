491fac8aeaa313a0ad9c209cbd7a68a8406d9cf7
maripen <GIT525da>
2021-08-29 19:25:19 +0200

    Initial commit - docker - mongo - project skeleton

---
+++ src/main/java/com/adverity/dwh/DwhApplication.java
@@ -0,0 +1,13 @@
+package com.adverity.dwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adverity/dwh/DwhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adverity.dwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
